9b0c76e1170660b5383a4a0f5eee67744933c6f3
biz-Aanchal <98saanchal@gmail.com>
2023-04-19 03:11:31 +0530

    done

---
+++ src/Pages/main.js
@@ -0,0 +1,339 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MovieFilterIcon from "@mui/icons-material/MovieFilter";
+import LocalMoviesIcon from "@mui/icons-material/LocalMovies";
+import MenuIcon from "@mui/icons-material/Menu";
+import TranslateIcon from "@mui/icons-material/Translate";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Carousel as MainCarousel } from "react-responsive-carousel"; //
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Data } from "../util/Data";
+import { Button, Grid, TextField } from "@mui/material";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const drawerWidth = 180;
+const StdbgColor = "#F0F8FF";
+
+function Layout(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      {/* <Toolbar /> */}
+      <div
+        style={{
+          minHeight: "64px",
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+        }}
+      >
+        <Typography variant="h6" noWrap component="div">
+          PRO OTT
+        </Typography>
+      </div>
+
+      <Divider />
+      <List>
+        {["Channels", "Languages", "Genres"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index == 0 ? (
+                  <MovieFilterIcon />
+                ) : index == 1 ? (
+                  <TranslateIcon />
+                ) : (
+                  <LocalMoviesIcon />
+                )}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex", backgroundColor: StdbgColor, color: "black" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          backgroundColor: StdbgColor,
+          color: "black",
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {/* <Typography variant="h6" noWrap component="div"> */}
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={1}
+          >
+            <Grid item xs={7} lg={6.5}>
+              <Grid container>
+                {[
+                  "Home",
+                  "Shows",
+                  "Movies",
+                  "Comedy",
+                  "News",
+                ].map((index) => {
+                  return (
+                    <Grid item xs={2.2}>
+                      {index}
+                      {/* sx={{display:{xs:'none', md:''}}} */}
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Grid>
+            <Grid item xs={2.5} lg={2}>
+              <TextField
+                id="outlined-basic"
+                label="Search"
+                variant="outlined"
+                size="small"
+              />
+            </Grid>
+            <Grid item xs={1} lg={1.5}>
+              <Button>Login</Button>
+            </Grid>
+            <Grid item xs={1.5} lg={1.5}>
+              <Button>Subscribe</Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          className="Bg-color App-link"
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: StdbgColor,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          className="Bg-color App-link"
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: StdbgColor,
+              boxShadow:
+                "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+
+        {/* Main Carousel- Image slider */}
+        <Box sx={{ height: "20%", overflow: "hidden", marginBottom: "4%" }}>
+          <MainCarousel
+            infiniteLoop
+            autoPlay={true}
+            //   centerMode={true}
+            showThumbs={false}
+            // dynamicHeight={true}
+            style={{
+              height: "100px !important",
+              ".carousel-root .carousel-slider": {
+                height: "100px !important",
+              },
+            }}
+          >
+            {Data?.sliderImages.map((row, index) => {
+              const { src, description, label } = row;
+              return (
+                <div key={index} class='main-carousel'>
+                  <img src={src} style={{ height: "25em" }} />
+                  <span
+                    className="legend"
+                    style={{
+                      position: "absolute",
+                      top: "40%",
+                      height: "fit-content",
+                      width: "80%",
+                      background: "transparent",
+                      textAlign: "left",
+                    }}
+                  >
+                    <Typography variant="h2"> {label}</Typography>
+                    <h3> {description}</h3>
+                    <Button variant="contained">Watch Now</Button>
+                  </span>
+                </div>
+              );
+            })}
+          </MainCarousel>
+        </Box>
+
+        {/* Recommended for you */}
+        <Box sx={{ marginBottom: "40px" }}>
+          <h2>Recommended for you</h2>
+          <CarouselFunction array={Data?.Recommended} />
+        </Box>
+
+        {/* Animated Adventures */}
+        <Box sx={{ marginBottom: "40px" }}>
+          <h2>Animated Adventures</h2>
+          <CarouselFunction array={Data?.AnimatedAdventures} />
+        </Box>
+
+        {/* Popular Shows/Drama */}
+        <Box sx={{ marginBottom: "40px" }}>
+          <h2>Popular Shows/Drama</h2>
+          <CarouselFunction array={Data?.Popular} />
+        </Box>
+
+        {/* New & Upcoming */}
+        <Box sx={{ marginBottom: "40px" }}>
+          <h2>New & Upcoming</h2>
+          <CarouselFunction array={Data?.NewUpcoming} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+Layout.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default Layout;
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const CarouselFunction = (props) => {
+  const { array } = props;
+  return (
+    <Carousel ssr partialVisbile itemClass="image-item" responsive={responsive}>
+      {array?.map((image) => {
+        return array?.map((row, index) => {
+          const { src, description, label } = row;
+          return (
+            <div
+              key={index}
+              class="clild-carousel"
+            >
+              <img src={src} style={{ width: "20em", height: "15em" }} />
+              <span
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  top: 10,
+                  width: "100%",
+                  textAlign: "center"
+                }}
+              >  <Typography variant="h5"> {label}</Typography>
+              </span>
+              <span
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  bottom: 0,
+                  width: "100%",
+                  textAlign: "center",
+                  padding: "20px",
+                }}
+              >
+                {" "}
+                <Typography variant="body2"> {description}</Typography>{" "}
+              </span>
+            </div>
+          );
+        });
+      })}
+    </Carousel>
+  );
+};
---
+++ src/util/Data.js
@@ -0,0 +1,116 @@
+export const Data = {
+  sliderImages: [
+    {
+      id: 0,
+      label: "Mrs. Undercover",
+      src: "/mrs-undercover.jpg",
+      iconName: "LocalMoviesIcon",
+      description:
+        "A simple Indian housewife who is in fact a special undercover agent is called back on the job after 10 years of being undercover to take down a terrifying, psycho killer while maintaining her parallel life of a housewife.",
+    },
+    {
+      id: 0,
+      label: "Plane",
+      src: "/plane.jpeg",
+      description:
+        "A simple Indian housewife who is in fact a special undercover agent is called back on the job after 10 years of being undercover to take down a terrifying, psycho killer while maintaining her parallel life of a housewife.",
+    },
+    {
+      id: 0,
+      label: "Guilty Minds",
+      src: "/guilty-minds.jpg",
+      description:
+        "Guilty Minds is a legal drama about one family that is the paragon of virtue and the other, a leading law firm dealing with all shades of grey.",
+    },
+    {
+        id: 0,
+        label: "Luck",
+        src: "/luck.jpg",
+        iconName: "LocalMoviesIcon",
+        description:"The story of Sam Greenfield, the unluckiest person in the world. Suddenly finding herself in the never-before-seen Land of Luck, she must unite with the magical creatures there to turn her luck around."
+      },
+  ],
+  Recommended: [
+    {
+      id: 0,
+      label: "Smile",
+      src: "/smile.jpg",
+      iconName: "LocalMoviesIcon",
+      description:
+        "A simple Indian housewife who is in fact a special undercover agent is called back on the job after 10 years of being undercover to take down a terrifying, psycho killer while maintaining her parallel life of a housewife.",
+    },
+    {
+      id: 0,
+      label: "Harry Potter",
+      src: "/harry-potter-1.jpg",
+      description:"Harry Potter, an eleven-year-old orphan, discovers that he is a wizard and is invited to study at Hogwarts. Even as he escapes a dreary life and enters a world of magic, he finds trouble awaiting him." },
+    {
+      id: 0,
+      label: "Guilty Minds",
+      src: "/guilty-minds.jpg",
+      description:
+        "Guilty Minds is a legal drama about one family that is the paragon of virtue and the other, a leading law firm dealing with all shades of grey.",
+    },
+  ],
+  AnimatedAdventures: [
+    {
+      id: 0,
+      label: "Luck",
+      src: "/luck.jpg",
+      iconName: "LocalMoviesIcon",
+      description:"The story of Sam Greenfield, the unluckiest person in the world. Suddenly finding herself in the never-before-seen Land of Luck, she must unite with the magical creatures there to turn her luck around."
+    },
+    {
+      id: 0,
+      label: "Puss In Boots",
+      src: "/puss-in-boots.webp",
+      description:"Puss in Boots discovers that his passion for adventure has taken its toll when he learns that he has burnt through eight of his nine lives. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives."},
+    {
+      id: 0,
+      label: "The Boss Baby: Get That Baby!",
+      src: "/boss-baby.jpg",
+      description:"Think you've got what it takes to be the boss? This interactive special puts your skills to the test and matches you up with one of 16 jobs at Baby Corp." },
+  ],
+  Popular: [
+    {
+      id: 0,
+      label: "Friends",
+      src: "/friends.jpg",
+      iconName: "LocalMoviesIcon",
+      description:"Follow the lives of six reckless adults living in Manhattan, as they indulge in adventures which make their lives both troublesome and happening."
+    },
+    {
+      id: 0,
+      label: "Gilmore Girls",
+      src: "/gilmore-girls.jpg",
+      description:"Lorelai Gilmore, an independent young woman, shares a close bond with her wilful daughter, Rory. However, her relationship with her own mother is strained."
+    },
+    {
+      id: 0,
+      label: "Guilty Minds",
+      src: "/guilty-minds.jpg",
+      description:
+        "Guilty Minds is a legal drama about one family that is the paragon of virtue and the other, a leading law firm dealing with all shades of grey.",
+    },
+  ],
+  NewUpcoming: [
+    {
+      id: 0,
+      label: "Spider-Man: Across the Spider-Verse",
+      src: "/spider-man.webp",
+      iconName: "LocalMoviesIcon",
+      description:"After reuniting with Gwen Stacy, Brooklyn's full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence."},
+    {
+      id: 0,
+      label: "Elemental",
+      src: "/elemental.jpg",
+      description:"In a city where fire, water, land, and air residents live together, a fiery young woman and a go-with-the-flow guy discover something elemental: how much they actually have in common."
+    },
+    {
+      id: 0,
+      label: "Aquaman and the Lost Kingdom",
+      src: "/aquaman.jpg",
+      description:"Aquaman forges an uneasy alliance with an unlikely ally in a bid to save Atlantis and the rest of the planet."
+     },
+  ],
+};
